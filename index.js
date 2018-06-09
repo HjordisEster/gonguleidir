da7df5cef9eb31b4cddc476e3e6e9b9a935659b8
HjordisEster <hjordis.ester@gmail.com>
2018-06-09 17:14:50 +0000

    trying out express (#1)

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 import express from "express";
+import birds from "./birds";
 
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/about", (req, res) => res.send("about"));
+
+app.use("/birds", birds);
+
+app.get("/walks/:id(d+)", (req, res) => {
+  console.log(req.params);
+  res.send(req.params.id);
+});
+
+let logHandler = (req, res, next) => {
+  console.log("the response will be sent by the next function ...");
+  next();
+};
+
+let responseHandler = (req, res) => {
+  res.json('{"name": "responseHandler"}');
+};
+
+app.get("/example/b", [logHandler, responseHandler]);
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
